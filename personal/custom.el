b3c6c72e0e07234626bda8abe596333528b8810f
Brent Sanders <git@thoughtnoise.net>
2011-11-19 02:11:54 -0800

    beginnings of adding personal init

---
+++ personal/custom.el
@@ -0,0 +1,39 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(completions-format (quote vertical))
+ '(custom-safe-themes (quote ("6b076aec76f8e203ec05ca6ac05564c6e2594adc" "171e4911e28356ec77a6fc2e5706eea9724c9b5f" default)))
+ '(fringe-mode (quote (4 . 4)) nil (fringe))
+ '(global-hl-line-mode nil)
+ '(global-pretty-lambda-mode t)
+ '(ido-enable-dot-prefix t)
+ '(ido-enable-prefix t)
+ '(ido-everywhere t)
+ '(ido-record-ftp-work-directories nil)
+ '(indicate-empty-lines t)
+ '(inhibit-startup-echo-area-message "endymion")
+ '(initial-scratch-message nil)
+ '(kill-do-not-save-duplicates t)
+ '(mouse-avoidance-mode (quote banish) nil (avoid))
+ '(prelude-disable-arrow-navigation nil)
+ '(read-buffer-completion-ignore-case t)
+ '(read-file-name-completion-ignore-case t)
+ '(require-final-newline t)
+ '(ruby-block-highlight-toggle t)
+ '(show-paren-mode t)
+ '(show-paren-style (quote parenthesis))
+ '(tool-bar-mode nil)
+ '(visual-line-fringe-indicators (quote (left-curly-arrow right-curly-arrow)))
+ '(whitespace-action (quote (auto-cleanup warn-if-read-only)))
+ '(windmove-wrap-around t)
+ '(xterm-mouse-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:stipple nil :background "#0b0b0b" :foreground "#92b6f4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 87 :width normal :foundry "xos4" :family "Terminus")))))
